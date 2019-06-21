1b56340f116b54de09b5c4c5aec62dede3aa31db
Forrest Leung <fleung48@gmail.com>
2019-06-21 13:45:24 -0700

    ESP8266 Connection Verification

---
+++ ESP8266.ino
@@ -0,0 +1,46 @@
+// This example shows how to connect to Cayenne using an ESP8266 and send/receive sample data.
+// Make sure you install the ESP8266 Board Package via the Arduino IDE Board Manager and select the correct ESP8266 board before compiling. 
+
+//#define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+#include <CayenneMQTTESP8266.h>
+
+// WiFi network info.
+char ssid[] = "forrest";  
+char wifiPassword[] = "testme21"; 
+
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = "a0105d20-8d5e-11e9-b4eb-6bf2c2412b24";
+char password[] = "da33a4def7d97b6a864a5501d42903a8dc2d511f";
+char clientID[] = "b0505e10-8d5e-11e9-ace6-4345fcc6b81e";
+
+unsigned long lastMillis = 0;
+
+void setup() {
+	Serial.begin(9600);
+	Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+}
+
+void loop() {
+	Cayenne.loop();
+}
+
+// Default function for sending sensor data at intervals to Cayenne.
+// You can also use functions for specific channels, e.g CAYENNE_OUT(1) for sending channel 1 data.
+CAYENNE_OUT_DEFAULT()
+{
+	// Write data to Cayenne here. This example just sends the current uptime in milliseconds on virtual channel 0.
+	Cayenne.virtualWrite(0, millis());
+	// Some examples of other functions you can use to send data.
+	//Cayenne.celsiusWrite(1, 22.0);
+	//Cayenne.luxWrite(2, 700);
+	//Cayenne.virtualWrite(3, 50, TYPE_PROXIMITY, UNIT_CENTIMETER);
+}
+
+// Default function for processing actuator commands from the Cayenne Dashboard.
+// You can also use functions for specific channels, e.g CAYENNE_IN(1) for channel 1 commands.
+CAYENNE_IN_DEFAULT()
+{
+	CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
+	//Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
+}
