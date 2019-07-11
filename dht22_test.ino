170fc7f93f9af70412b22526cb337cec7c8d1360
Forrest Leung <fleung48@gmail.com>
2019-07-11 10:27:05 -0700

    Testing DHT22 inputs
    Realized my DHT sensor was not an 11, but 22

---
+++ dht22_test.ino
@@ -0,0 +1,28 @@
+#include <DHT.h>;
+
+#define DHTPIN D2     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop()
+{
+    delay(2000);
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    delay(10000);
+}
